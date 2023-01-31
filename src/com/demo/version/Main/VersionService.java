476662de1d8289ff6c234ae0aa3728e0a361b240
sangeetha170494 <123965667+sangeetha170494@users.noreply.github.com>
2023-01-30 18:52:02 -0800

    Update VersionService.java

--- src/com/demo/version/Main/VersionService.java
+++ src/com/demo/version/Main/VersionService.java
@@ -12,25 +12,28 @@ public class VersionService {
 
 
 	public static int versionCompare(String str1, String str2) {
-		try {
 	      String [] ver1 = str1.split("\\.");
 	      String [] ver2 =  str2.split("\\.");
-	      
+
+	      /* Checking the empty String and
+	      matches only the digits and dot
+	      */
+              try{	
 	      if(StringUtils.isNotEmpty(str2) && StringUtils.isNotEmpty(str1) && str1.matches("^\\d+(\\.\\d+)+$") && str2.matches("^\\d+(\\.\\d+)+$")) {
-          for(int i =0; i < ver1.length && i <  ver2.length; i++) {         
-		        int v1 = Integer.parseInt(ver1[i]);
+              for(int i =0; i < ver1.length && i <  ver2.length; i++) {         
+		    int v1 = Integer.parseInt(ver1[i]); // convert the String into Integer
 	            int v2 = Integer.parseInt(ver2[i]);
+		      //check the condition for greater and less than of two versions
 	            if (v1 < v2) {
 	                return -1;
 	            } else if (v1 > v2) {
 	                return 1;
 	            }
 	        }
-	      }
-	        
-	    } 
+	      }     
+	    } // throws the exception
 		catch (Exception e) {
-			System.out.println(e);
+			throw new IllegalArgumentException("Invalid Argument: "+ e);
 		}
 		return 0;
 		
